4fa2fce2e57d1886b7b2c9010285057e586171e2
T11 <mironov@n1.by>
2022-09-29 15:44:12 +0300

    if 5 < x < 15

--- pythonmanual/temp.py
+++ pythonmanual/temp.py
@@ -1,11 +1,4 @@
-if __name__ == "__main__":
-    pass
+x = 14
 
-for i in range(10):
-    try:
-        if 10/i == 2.0:
-            break
-    except ZeroDivisionError:
-        print(1)
-    else:
-        print(2)
+if 5 < x < 15:
+    print('Ok')
